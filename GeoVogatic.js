ed0b128f09e1c54c3b719d11c049c002f35e7f33
Ayah93 <ayah.hyari@yahoo.com>
2017-01-10 22:59:45 +0100

    Updating GeoVogatic.js
    This code has the functions of the help button, remove button, close button and the working functions for changing the styling of the shapefiles of Point, Line and Polygon.

--- GeoVogatic.js
+++ GeoVogatic.js
@@ -1,27 +1,57 @@
 var temp;
 var i = 1;
+a=0;
 
+function hide(target) {
+    document.getElementById(target).style.display = 'none';
+}
 //Function for button title
 function button_style(temp) {
 	switch(temp){
-		case 'Point': 
+		case 'Point':
 		case 'MultiPoint':{
 			while(i == 1){
 				var buttonlayer = document.createElement('button');
 					buttonlayer.setAttribute('class', 'layers');
 					buttonlayer.innerHTML = 'Point';
-
+					
+					$("buttonlayer").click(function() {
+						var $this = $(this);
+						if ($this.hasClass("clicked-once")) {
+							// already been clicked once, hide it
+							$this.hide();
+						}
+						else {
+							// first time this is clicked, mark it
+							$this.addClass("clicked-once");
+						}
+					});
 				var div = document.getElementsByTagName('div')[0];
 					div.appendChild(buttonlayer);
 					buttonlayer.addEventListener("click", function (){
 						if (temp == 'Point' || temp == 'MultiPoint' )
 						{	$('#myPoint').css('display','block');
 						    $('#myLine').css('display','none');
-						    $('#myPolygon').css('display','none');     
-						}		
-						else 
-							alert ("add a GeoJSON or GML");
-			       });			
+						    $('#myPolygon').css('display','none');
+						}
+					});
+			//remove button and function
+				var remove = document.createElement('button');
+					remove.setAttribute('class', 'remo');
+					remove.innerHTML = 'X';
+				var div = document.getElementsByTagName('div')[0];
+					div.appendChild(remove);
+					remove.addEventListener("click", function (){
+						var list=document.getElementsByClassName("remo");
+							list = [].slice.call(list); 
+						var a = list.indexOf(remove);
+							console.log(a);
+						
+						map.removeLayer(map.getLayers().item(a+1));
+						$( ".layers" )[a].remove();
+						$(".remo")[a].remove();
+						$("#myPoint").hide();
+			       });
 				i++;
 					}
 				break;
@@ -40,17 +70,33 @@ function button_style(temp) {
 						{    $('#myPoint').css('display','none');
 							 $('#myLine').css('display','block');
 							 $('#myPolygon').css('display','none');
-						}							 
-						else 
-							alert ("add a GeoJSON or GML");
+						}
+						});
+
+			//remove button and function	
+				var remove = document.createElement('button');
+					remove.setAttribute('class', 'remo');
+					remove.innerHTML = 'X';
+				var div = document.getElementsByTagName('div')[0];
+					div.appendChild(remove);
+					remove.addEventListener("click", function (){
+						var list=document.getElementsByClassName("remo");
+							list = [].slice.call(list); 
+						var a = list.indexOf(remove);
+							console.log(a);
+						
+						map.removeLayer(map.getLayers().item(a+1));
+						$( ".layers" )[a].remove();
+						$(".remo")[a].remove();
+						$("#myLine").hide();
 					});
-							
+
 					i++;
 					}
 				break;
 			};
 		case 'Polygon':
-		case 'MultiPolygon':{ 
+		case 'MultiPolygon':{
 			while(i == 1){
 				var buttonlayer = document.createElement('button');
 					buttonlayer.setAttribute('class', 'layers');
@@ -59,14 +105,32 @@ function button_style(temp) {
 				var div = document.getElementsByTagName('div')[0];
 					div.appendChild(buttonlayer);
 					buttonlayer.addEventListener("click", function (){
-					if ( temp == 'MultiPolygon' || temp == 'Polygon' ) 
+					if ( temp == 'MultiPolygon' || temp == 'Polygon' )
 					{        $('#myPoint').css('display','none');
 							 $('#myLine').css('display','none');
-							 $('#myPolygon').css('display','block');   
-				    } 
-					else 
+							 $('#myPolygon').css('display','block');
+				    }
+					else
 						alert ("add a GeoJSON or GML");
-						});				
+						});
+						
+				//remove button and function
+					var remove = document.createElement('button');
+						remove.setAttribute('class', 'remo');
+						remove.innerHTML = 'X';
+					var div = document.getElementsByTagName('div')[0];
+						div.appendChild(remove);
+						remove.addEventListener("click", function (){
+						var list=document.getElementsByClassName("remo");
+							list = [].slice.call(list); 
+						var a = list.indexOf(remove);
+						console.log(a);
+						
+						map.removeLayer(map.getLayers().item(a+1));
+						$( ".layers" )[a].remove();
+						$(".remo")[a].remove();
+						$("#myPolygon").hide();
+						});
 					i++;
 					}
 				break;
@@ -80,54 +144,52 @@ function button_style(temp) {
 			'Point': new ol.style.Style({
 				image: new ol.style.Circle({
 				fill: new ol.style.Fill({
-					color: 'yellow'
-						}),
-				radius: 5,
+					color: $('#pcolor').val(),
+					}),
+				radius: $('#psize').val(),
 				stroke: new ol.style.Stroke({
-					color: '#ff0',
-					width: 1
+					color: $('#polc').val(),
+					width: $('#pwidth').val(),
 						})
 					})
 				}),
 			'LineString': new ol.style.Style({
 				stroke: new ol.style.Stroke({
-				color: '#f00',
-				width: 3
-					})
+				color: $('#Lcolor').val(),
+				width: Number($('#Lwidth').val())
+					}),
 				}),
 			'Polygon': new ol.style.Style({
 				fill: new ol.style.Fill({
-				color: 'rgba(0,255,255,0.5)'
+					color: $('#color').val(),
 					}),
 				stroke: new ol.style.Stroke({
-				color: '#0ff',
-				width: 1
+				color: $ ("#bg").val(),
+				width: $("#size").val()
 					})
 				}),
 			'MultiPoint': new ol.style.Style({
 			  image: new ol.style.Circle({
 				fill: new ol.style.Fill({
-				color: 'rgba(255,0,255,0.5)'
+				color: $('#pcolor').val(),
 					}),
-				radius: 5,
+				radius: $('#spize').val(),
 				stroke: new ol.style.Stroke({
-					color: '#f0f',
-					width: 1
+					color: $('#polc').val(),
+					width: $('#pwidth').val(),
 						})
 					})
 				}),
 			'MultiLineString': new ol.style.Style({
 				stroke: new ol.style.Stroke({
-				color: '#0f0',
-				width: 3
-						})
+				color: $('#Lcolor').val(),
+				width: $('#Lwidth').val()
+						}),
 					}),
 			'MultiPolygon': new ol.style.Style({
-				fill: new ol.style.FillPattern({
-					pattern: $('#pselect'),
-					color: $("#color").val(),
+				fill: new ol.style.Fill({
+					color: $('#color').val(),
 					
-					 
 					}),
 				stroke: new ol.style.Stroke({
 				color: $ ("#bg").val(),
@@ -135,13 +197,13 @@ function button_style(temp) {
 						})
 					})
 				};
-
-// Style Function => This is the link between the GeoJSON file and the default style 			  
-	var styleFunction = function(feature, resolution) {			 
+				var buttonText;
+// Style Function => This is the link between the GeoJSON file and the default style
+	var styleFunction = function(feature, resolution) {
 		return defaultStyle[feature.getGeometry().getType()];
-	}; 
-		
-//Drag and drop Function => allows the map to load the given extensions	
+	};
+
+//Drag and drop Function => allows the map to load the given extensions
     var dragAndDropInteraction = new ol.interaction.DragAndDrop({
         formatConstructors: [
 			ol.format.GeoJSON,
@@ -149,7 +211,7 @@ function button_style(temp) {
 			]
 		});
 
-//Map => creates the map     
+//Map => creates the map
 	var map = new ol.Map({
         interactions: ol.interaction.defaults().extend([dragAndDropInteraction]),
         layers: [
@@ -161,98 +223,129 @@ function button_style(temp) {
           zoom: true,
           attribution: true,
           rotate: true,
-        }),	
+        }),
         target: 'map',
         view: new ol.View({
           center: [0, 0],
           zoom: 2,
         })
     });
-	
-//  Drag and Drop interaction Function => describes how to respond once the file is dragged and dropped   
+
+//  Drag and Drop interaction Function => describes how to respond once the file is dragged and dropped
 	dragAndDropInteraction.on('addfeatures', function(event) {
 		i = 1;
+		window.features = event.features;
 	    button_style(event.features[0].getGeometry().getType()); // Creates a button
-        var vectorSource = new ol.source.Vector({
+        window.vectorSource = new ol.source.Vector({
 			features: event.features
 			});
-        map.addLayer(new ol.layer.Vector({
+		window.layer =new ol.layer.Vector({
 			source: vectorSource,
 			style: styleFunction,
-			}));
+			});
+        map.addLayer(layer);
         map.getView().fit(
-            vectorSource.getExtent(),(map.getSize())); 
+            vectorSource.getExtent(),(map.getSize()));
 
 
 	});
-// Function to call the pattern and the Refresh function: 
 
-var refreh;
-						
-		$(window).load (function()
-		{	// AddChar patterns
-			ol.style.FillPattern.addPattern ("copy (char pattern)", { char:"©" });
-			ol.style.FillPattern.addPattern ("bug (fontawesome)", { char:'\uf188', size:12, font:"10px FontAwesome" });
-			ol.style.FillPattern.addPattern ("smiley (width angle)", { char:'\uf118', size:20, angle:true, font:"15px FontAwesome" });
+// Get the popup for the help button 
+		var Popup = document.getElementById('HelpPopup');
 
-			// Popup
-			$("#select").click(function()
-			{	$("#pselect").toggle();
-			});
+		// Get the button that opens the popup
+		var btn = document.getElementById("HelpButton");
 
-		var pat = "hatch"
-		var spattern = $("#pselect");
-		for (var i in ol.style.FillPattern.prototype.patterns)
-		{	var p = new ol.style.FillPattern({ pattern:i })
-			$("<div>").attr('title',i)
-			.css("background-image",'url("'+p.getImage().toDataURL()+'")')
-			.click(function(){ pat = $(this).attr("title"); refresh(); })
-			.appendTo(spattern);
-							}
-			var p = new ol.style.FillPattern({ image: new ol.style.Icon({ src : 'pattern.png' }) });
-			$("<div>").attr('title','Image (PNG)')
-				.css("background-image",'url("pattern.png")')
-				.click(function(){ pat = $(this).attr("title"); refresh(); })
-				.appendTo(spattern);					
-					 
-			function refresh() {
-				debugger;
-				$('#pselect').hide();
-				var p = new ol.style.FillPattern(
-						{	pattern: pat,
-							ratio: 2,
-							color: '#000',
-							size: Number($("#size").val()),
-							spacing: Number($("#spacing").val()),
-							angle: Number($("#angle").val())
-						});
-			$("#select").css('background-image', 'url('+p.getImage().toDataURL()+')'); 
-				$('#color')
+		// Get the <span> element that closes the popup
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the popup
+		btn.onclick = function() {
+		Popup.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the popup
+		span.onclick = function() {
+		Popup.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the popup, close it
+		window.onclick = function(event) {
+		if (event.target == Popup) {
+			Popup.style.display = "none";
+		}
+		};
+
+
+// function to change the style of polygons 
+	function updateStyle(){
 				
-			};		 
-
+				
+		  		var polystroke = new ol.style.Stroke ({
+					color : $('#bg').val(),
+					width: $('#size').val()
+				});					
+				var polystyle = new ol.style.Fill ({
+					color: $('#color').val()
+				});
+				
+							
+				var style = new ol.style.Style({
+					fill: polystyle,
+					stroke: polystroke,
+				});
 					
-					// this part has been removed
-					//	vector.getSource().addFeature(new ol.Feature(new ol.geom.Polygon() ) );
-					debugger;
-					refresh();
-						});
-		
 
+                window.layer.setStyle(style);
+               map.getView().fit(
+               vectorSource.getExtent(),(map.getSize()));
 
-// This is the function for the transparency scrollbar 
-		$(document).ready(function(e) {
-		
-			$("#myrange_point").change(function(e) {
-				$("h1").html(this.value);
-				$("img").css("opacity",this.value);  // this is where we'll link the layer to change the transparency for the point layer
-			});
-			$("#myrange_polygon").change(function(e) {
-				$("h2").html(this.value);
-				$("img").css("opacity",this.value);  // this is where we'll link the layer to change the transparency for the polygon layaer
-			});
-			$("#myrange_line").change(function(e) {
-				$("h3").html(this.value);
-				$("").css("opacity",this.value);  // this is where we'll link the layer to change the transparency for the line layaer
-			});
+	};
+// function to change the style of points
+	function updatePoint() {
+		var ppfill = new ol.style.Fill ({
+			color : $('#pcolor').val()
+			//opacity: 
 		});
+		
+		var ppstroke = new ol.style.Stroke ({
+			color : $('#polc').val(),
+			width: $('#pwidth').val()
+		});
+		
+		var pp = new ol.style.Circle ({
+				fill : ppfill,
+		
+				radius: $('#psize').val(),
+				stroke : ppstroke,
+		});
+		
+		
+		
+		var style = new ol.style.Style({
+			image: pp,
+		});
+		
+		window.layer.setStyle(style);
+		map.getView().fit(
+        vectorSource.getExtent(),(map.getSize()));
+	};
+// function to change the style of lines	
+	function updateLine() {
+		
+		var linestyle = new ol.style.Stroke ({
+				color: $('#Lcolor').val(),
+				width:Number( $('#Lwidth').val()),
+				});
+		
+		var style3 = new ol.style.Style ({
+			stroke: linestyle,
+		});
+		linestyle,
+		
+		window.layer.setStyle(style3);
+		map.getView().fit(
+        vectorSource.getExtent(),(map.getSize()));
+	};
+	
+	
